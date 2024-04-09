e81d5293ea13dd1958489711a7570a76445ed594
EGet21@% <elisha.solowey@gmail.com>
2024-04-09 18:43:48 +0300

    welcom

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-print("wellcom to the class of 1997")
+print("welcom to the class of 1997")
 x=int(input("enter a number"))
 y=int(input("enter a number"))
 print(f"{x} + {y} = {x+y}")
 print(f"{x} - {y} = {x-y}")
+ 
